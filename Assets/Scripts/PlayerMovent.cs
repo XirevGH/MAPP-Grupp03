2c60ad22cc15ada9512d38caa22c0da1bfb86c95
asawinSit <asawin.sitthi@outlook.com>
2024-04-09 12:25:52 +0200

    playermovent

---
+++ Assets/Scripts/PlayerMovent.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 25f;
+    [SerializeField] private Transform pos;
+
+    private float horizontalValue;
+    private float verticalValue;
+    public bool canMove;
+ 
+
+
+    void Start()
+    {
+        
+        canMove = true;
+        
+        
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale == 0f)
+        {
+            return;
+        }
+
+
+        if (!canMove) return;
+
+        horizontalValue = Input.GetAxis("Horizontal");
+        verticalValue = Input.GetAxis("Vertical");
+  
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        if (!canMove)
+            return;
+
+        pos.position = new Vector2(pos.position.x + horizontalValue * moveSpeed * Time.deltaTime, pos.position.y + verticalValue * moveSpeed * Time.deltaTime);
+
+    }
+
+
+
+
+
+
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
