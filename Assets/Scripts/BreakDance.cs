ac35ddd950cfc17d6db3c3dfc8bbf9dff4d1f516
XirevGH <dcz95@hotmail.com>
2024-04-10 14:29:57 +0200

    Created Weapon superclass
    Added methods to generalize cooldown handling and damage dealing for all types of weapons

--- Assets/Scripts/BreakDance.cs
+++ Assets/Scripts/BreakDance.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakDanceAbility : MonoBehaviour
+public class BreakDance : Weapon
 {
     public float damage;
     public bool usedAbility = false;
