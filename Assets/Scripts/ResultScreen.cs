0511664a5970bcbbc2939ecf6223d91b7d5c8c72
ElvisSkl <elvisforsell@gmail.com>
2024-04-11 14:42:21 +0200

    Added some soundeffects

---
+++ Assets/Scripts/ResultScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultScreen : MonoBehaviour
+{
+    [SerializeField] private int levelToload;
+    [SerializeField] private GameObject exit;
+    [SerializeField] private TextMeshProUGUI enemiesKilledText;
+
+    
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(levelToload);
+    }   
+
+}
