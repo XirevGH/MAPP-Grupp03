655780f02da23aab326fa453cc8f364348136e65
XirevGH <dcz95@hotmail.com>
2024-04-11 18:22:46 +0200

    Updated save function

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private string saveFile;
     public Player playerPrefab;
     private Player playerFromSave;
 
-    private void Start()
+    private void Awake()
     {
         string saveFile = Application.persistentDataPath + "/playerInfo.json";
-        playerFromSave = Player.CreateFromJSON(saveFile);
-        playerPrefab = Instantiate(playerPrefab, new Vector3(0f, 0f), Quaternion.identity);
-        playerPrefab = playerFromSave;
+        ReadFile();
+    }
+
+    private void ReadFile()
+    {
+        if (File.Exists(saveFile))
+        {
+            string fileContents = File.ReadAllText(saveFile);
+            playerFromSave = Player.CreateFromJSON(fileContents);
+            playerPrefab = Instantiate(playerPrefab, new Vector3(0f, 0f), Quaternion.identity);
+            playerPrefab = playerFromSave;
+        }
+        else
+        {
+            Debug.Log("File does not exist.");
+        }
     }
 
     public void GameOver()
     {
-        playerPrefab.GetComponent<Player>().SaveToString();
+        File.WriteAllText(saveFile, playerPrefab.GetComponent<Player>().SaveToString());
     }
 }
