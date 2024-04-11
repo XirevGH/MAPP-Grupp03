b99514b6dbe70577762288c721c6696eefc2f0ba
blenda-a <barnebjer@gmail.com>
2024-04-11 16:05:15 +0200

    Script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float followSpeed = 2f;
+    public float yOffset = 1f;
+    public transform target; 
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        transform.position = vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
     }
 }
