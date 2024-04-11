e1182ed4afee0dc370333617976315d5c4f0113a
FelixLieb <felix.liebgott@gmail.com>
2024-04-11 11:53:25 +0200

    Prefab och script
    +Saxophon script
    +NoteProjektil

---
+++ Assets/Scripts/SaxohoneWepon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaxohoneWepon : Weapon
+{
+    /*public bool dealDamage;
+    public float ;
+    public int amountOfHits;
+    private int hitsCounter;*/
+}
