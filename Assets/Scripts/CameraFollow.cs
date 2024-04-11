2ed0b1b22a8d331e5514eea71c6c3a9a5d08778d
blenda-a <barnebjer@gmail.com>
2024-04-11 16:14:12 +0200

    CameraFollow script
    Ändrade following speed

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float followSpeed = 2f;
+    public float followSpeed = 6f;
     public float yOffset = 1f;
     public Transform target; 
 
