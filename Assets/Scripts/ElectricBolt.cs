7f835be9f359e59533e210bfc3c2f73d02ca7b2b
BrownCheeseLord <lovewilhelm@gmail.com>
2024-04-11 15:07:02 +0200

    Bolt changes

--- Assets/Scripts/ElectricBolt.cs
+++ Assets/Scripts/ElectricBolt.cs
@@ -8,34 +8,74 @@ using UnityEngine.UIElements;
 public class ElectricBolt : MonoBehaviour
 {
     [SerializeField] private Sprite bolt1, bolt2;
-    [SerializeField] private GameObject guitar;
 
-    private GameObject enemy;
+    public GameObject startingUnit;
+    private GameObject targetUnit;
     private int bouncesLeft;
 
     private float length;
     private float angle;
     private Vector3 direction;
+    private float spriteChangeCooldown = 0.3f;
+    private bool spriteChangeReady;
+    private float lifetime;
 
     private SpriteRenderer rend;
 
     private void Start()
     {
         rend = GetComponent<SpriteRenderer>();
-        enemy = guitar.GetComponent<ElectricGuitar>().getEnemy();
-        bouncesLeft = guitar.GetComponent<ElectricGuitar>().getStartingBounces();
+        targetUnit = startingUnit.GetComponent<ElectricGuitar>().getEnemy();
+        bouncesLeft = startingUnit.GetComponent<ElectricGuitar>().getStartingBounces();
+        spriteChangeReady = true;
+        lifetime = startingUnit.GetComponent<ElectricGuitar>().getLifetime();
     }
 
     private void Update()
     {
-        length = Vector3.Distance(guitar.transform.position, enemy.transform.position);
-        guitar = guitar.gameObject;
-        enemy = guitar.GetComponent<ElectricGuitar>().getEnemy();
-        transform.position = (guitar.transform.position + enemy.transform.position) / 2f;
-        direction = enemy.transform.position - guitar.transform.position;
+        length = Vector3.Distance(startingUnit.transform.position, targetUnit.transform.position);
+        startingUnit = startingUnit.gameObject;
+        targetUnit = startingUnit.GetComponent<ElectricGuitar>().getEnemy();
+        direction = targetUnit.transform.position - startingUnit.transform.position;
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.position = startingUnit.transform.position;
         transform.rotation = Quaternion.Euler(0, 0, angle);
         transform.localScale = new Vector3(length / 10.24f, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void FixedUpdate()
+    {
+        lifetime -= Time.deltaTime;
+        if(lifetime < 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if(spriteChangeReady)
+        {
+            ChangeSprite();
+            spriteChangeReady = false;
+            Invoke("SetSpriteReady", spriteChangeCooldown);
+        }
+    }
+
+    private void ChangeSprite()
+    {
+        if(rend.sprite == bolt1)
+        {
+            rend.sprite = bolt2;
+        }
+        else
+        {
+            rend.sprite = bolt1;
+        }
+
+    }
+
+    private void SetSpriteReady()
+    {
+        spriteChangeReady = true;
     }
 
     //private IEnumerator Shoot()
