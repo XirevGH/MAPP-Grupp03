9b7b1e3af4f47a2d36cb42921b0b2b10dfa46fcb
FelixLieb <felix.liebgott@gmail.com>
2024-04-15 19:16:02 +0200

    Saxohon bullet animation
    + buelt animation color and zise
    + bullet sprite randomiser

--- Assets/Scripts/NoteProjectile.cs
+++ Assets/Scripts/NoteProjectile.cs
@@ -11,15 +11,28 @@ public class NoteProjectile : MonoBehaviour
 
     private Vector3 direction;
 
+    [SerializeField] private Sprite sprite1, sprite2;
     private float lifetime = 5f;
 
    public void Initialize(float damage, float speed, int startPenetration, Vector3 direction)
-{
-    this.damage = damage;
-    this.speed = speed;
-    this.penetration = startPenetration;  
-    this.direction = direction;
-}
+    {
+        this.damage = damage;
+        this.speed = speed;
+        this.penetration = startPenetration;  
+        this.direction = direction;
+        
+        GetComponent<SpriteRenderer>().sprite = randomSprite();
+        
+    }
+
+    private Sprite randomSprite(){
+        if(Random.Range(0, 2) == 1){
+            return sprite1;
+        }else{
+            return sprite2;
+        }
+    }
+    
 
     private void Update()
     {
