29ab60a61c2b1ef940b54f72aaa78d3578be1ebc
XirevGH <74151509+XirevGH@users.noreply.github.com>
2024-04-16 20:40:58 +0200

    Cleaned up PlayerMovement and added Aiming script

---
+++ Assets/Scripts/Aiming.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Aiming : MonoBehaviour
+{
+    public DynamicJoystick dynamicJoystick;
+    public Transform weapons;
+
+
+    private void FixedUpdate()
+    {
+        Debug.Log(dynamicJoystick.Horizontal + ", " + dynamicJoystick.Vertical);
+    }
+}
+}
