8b940f271475ad5ce0f85da58b46e2f5740a90e9
asawinSit <asawin.sitthi@outlook.com>
2024-04-18 20:02:52 +0200

    yolo

--- Assets/Scripts/Yoyo.cs
+++ Assets/Scripts/Yoyo.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 
 public class Yoyo : Weapon
 {
+    [SerializeField] private float rotateSpeed;
+    public float angle, superModeTime, elapsedTime, percentageComplete, superModeMultiplier;
+    private bool superMode;
 
-    public float angle;
-  
+    private void Start()
+    {
+        superMode = false;
+    }
     private void FixedUpdate()
     {
         if (angle == 360)
@@ -15,7 +20,27 @@ public class Yoyo : Weapon
          angle = 0;
         }
         this.transform.eulerAngles = new Vector3(0, 0, angle);
-        angle++;
+
+       
+        if (superMode)
+        {
+
+            angle += rotateSpeed * superModeMultiplier;
+            elapsedTime += Time.deltaTime;
+            percentageComplete = elapsedTime / superModeTime;
+            if (Mathf.FloorToInt(percentageComplete) == 1)
+            {
+                percentageComplete = 0;
+                elapsedTime = 0;
+                superMode = false;
+            }
+        }
+        else
+        {
+            angle += rotateSpeed;
+            superMode = false;
+        }
+        Debug.Log(superMode + "Supermode");
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -26,5 +51,11 @@ public class Yoyo : Weapon
             DealDamage(other);
         }
        
+    }
+
+    public override void Attack()
+    {
+        superMode = true;
+        StartCooldown();
     }
 }
