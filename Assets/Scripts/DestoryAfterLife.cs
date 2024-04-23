f1c72d3fafa02c94ce87087623371c907f2bba4d
blenda-a <144216398+blenda-a@users.noreply.github.com>
2024-04-23 16:36:29 +0200

    mainmenu
    text i main menu

---
+++ Assets/Scripts/DestoryAfterLife.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryAfterLife : MonoBehaviour
+{
+    [SerializeField] private float lifeTime = 1f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
---
+++ Assets/Scripts/TriggerPadController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class TriggerPadController : MonoBehaviour
+{
+
+    //[SerializeField] KeyCode keyCode;
+    //[SerializeField] GameObject currentWeapon;
+    [SerializeField] GameObject[] weapons;
+    [SerializeField] float reduceCooldownDuration;
+
+    //private SpriteRenderer spriteRenderer;
+    private GameObject beat;
+    //private bool isPressable;
+    //private int totalHit;
+
+    void Start()
+    {
+       // spriteRenderer = GetComponent<SpriteRenderer>();
+        //totalHit = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       // currentWeapon = GameObject.FindGameObjectWithTag("Weapon");
+
+        //    if (Input.GetKeyDown(keyCode))
+        //    {
+        //        spriteRenderer.color = Color.blue;
+        //        if (isPressable) 
+        //        {
+        //            Destroy(beat);
+        //            Debug.Log("Boogie Boogie");
+        //            ++totalHit;
+        //            currentWeapon.GetComponent<Weapon>().ChangeCooldownDuration(-0.0f);
+
+        //            Debug.Log(currentWeapon.GetComponent<Weapon>().GetCooldownDuration());
+
+        //        }
+        //    }
+
+        //    if (Input.GetKeyUp(keyCode))
+        //    {
+        //        spriteRenderer.color = Color.white;
+        //    }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        beat = collider.GameObject();
+        if (collider.CompareTag("Beat"))
+        {
+            foreach (GameObject weapon in weapons)
+            {
+                    weapon.GetComponent<Weapon>().ChangeCooldownDuration(-reduceCooldownDuration);  
+            }
+
+
+           
+            Destroy(beat);
+            //isPressable = true;
+        }
+    }
+
+    //void OnTriggerExit2D(Collider2D collider)
+    //{
+    //    if (collider.CompareTag("Beat"))
+    //    {
+    //        isPressable = false;
+    //    }
+    //}
+
+    
+}
