7af449754adfae88c4b4fece2c7600266dbdf386
ElvisSkl <elvisforsell@gmail.com>
2024-04-23 11:59:57 +0200

    slight tries to fix some major issues

--- Assets/Scripts/BreakDanceFix.cs
+++ Assets/Scripts/BreakDanceFix.cs
@@ -2,34 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakDance : Weapon
+public class BreakDanceFix : Weapon
 {
     public bool dealDamage;
     public int amountOfHits;
     public bool usedAbility = false;
     private int hitsCounter;
-    Collider2D other;   
+    Collider2D other;
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (WeaponIsReady()) {
-       //if(!usedAbility) { 
+            if(!usedAbility)
+            { 
             hitsCounter = amountOfHits;
             while (hitsCounter > 0)
-                {
-                    BreakDanceInterval();
-                }
-            StartCooldown();
-            if (hitsCounter <= 0) 
             {
-            usedAbility = true;
+                BreakDanceInterval();
+            }
+            StartCooldown();
+            if (hitsCounter <= 0)
+            {
+                usedAbility = true;
             }
         }
     }
 
     private void BreakDanceInterval()
     {
-        if (!dealDamage) 
+        if (!dealDamage)
         {
             dealDamage = true;
             Invoke("BreakDanceDamage", 0.5f);
@@ -41,19 +41,20 @@ public class BreakDance : Weapon
 
         DealDamage(other);
         hitsCounter--;
-      //  GetComponent<SpriteRenderer>().enabled = true;
+        GetComponent<SpriteRenderer>().enabled = true;
         dealDamage = false;
         Invoke("RemoveIndicator", 0.5f);
-     //   Invoke("AbilityCooldown", 3f);
+        Invoke("AbilityCooldown", 3f);
     }
 
     private void RemoveIndicator()
     {
-     //   GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
     }
 
     private void AbilityCooldown()
     {
-      usedAbility = false;
+        usedAbility = false;
     }
 }
+
