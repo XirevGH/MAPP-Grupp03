06ca1f1b25c5c65933f3e413fec912c5dfb200a2
asawinSit <asawin.sitthi@outlook.com>
2024-05-02 00:49:31 +0200

    disco ball blink

--- Assets/Scripts/Items/Weapons/ProjectileWeapons/DiscoBall.cs
+++ Assets/Scripts/Items/Weapons/ProjectileWeapons/DiscoBall.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DiscoBall : Projectile
 { 
     private Rigidbody2D rb;
-    public float height, distance;
-
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float height, distance, torque;
 
     private void Start()
     {
+        distance = Random.Range(-distance, distance);
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce( new Vector2 (Random.Range(-distance, distance), height));
+        rb.AddForce( new Vector2 (distance, height - Mathf.Abs(distance)));
+        rb.AddTorque(torque * Mathf.Sign(distance));
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.HSVToRGB(Random.Range(0f, 1f), 0.7f, 1 );
+       
+
+    }
+
+    public void Blink() 
+    {
+        
+          spriteRenderer.color = Color.HSVToRGB(Random.Range(0f, 1f), 0.7f, 1);
+       
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         DealDamage(other);
         DestroyWhenMaxPenetration();
+    }
+
+    public override void DestroyWhenMaxPenetration()
+    {
+        DiscoBallController.instance.activeDiscoBalls.Remove(gameObject);
+        Debug.Log("remove");
+        base.DestroyWhenMaxPenetration();
+
     }
 }
--- Assets/Scripts/Items/Weapons/ProjectileWeapons/DiscoBallController.cs
+++ Assets/Scripts/Items/Weapons/ProjectileWeapons/DiscoBallController.cs
@@ -1,10 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
 
 public class DiscoBallController : ProjectileWeapon
 {    
     [SerializeField] private GameObject discoBall;
+    [SerializeField] private float blinkTime;
+    public HashSet<GameObject> activeDiscoBalls = new HashSet<GameObject>();
+
+    public static DiscoBallController instance
+    {
+        get; private set;
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void Blink()
+    {
+        Debug.Log(activeDiscoBalls.Count);
+        if (activeDiscoBalls.Count != 0) 
+        {
+            foreach (GameObject discoBall in activeDiscoBalls)
+            {
+                discoBall.GetComponent<DiscoBall>().Blink();
+            }
+        }
+        
+    }
 
     public override void Attack()
     {
@@ -12,6 +48,7 @@ public class DiscoBallController : ProjectileWeapon
             for (int i = 0; i < amountOfProjectiles; i++) 
             {
                 GameObject clone = Instantiate(discoBall, transform.position, Quaternion.identity);
+                activeDiscoBalls.Add(clone);
                 clone.GetComponent<DiscoBall>().SetDamage(damage);
                 clone.GetComponent<DiscoBall>().SetPenetration(penetration);
             }
