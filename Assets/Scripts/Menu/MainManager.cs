52c0036b37c170bfc942c72aff7c3cbd895be401
FelixLieb <felix.liebgott@gmail.com>
2024-05-06 15:43:16 +0200

    Result scean återstäls näts gång man öpnar den

--- Assets/Scripts/Menu/MainManager.cs
+++ Assets/Scripts/Menu/MainManager.cs
@@ -14,8 +14,8 @@ public class MainManager : MonoBehaviour
 
     public static MainManager Instance;
 
-    public string timeText = "";
-    public int mainLevel = 1;
+    public string timeText;
+    public int mainLevel;
     public int enemiesDefeated;
     public int moneyEarned;
 
@@ -29,14 +29,21 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        Reset();
+    }
+    public void Reset() {
+        timeText = "";
+        mainLevel = 0;
+        enemiesDefeated = 0;
+        moneyEarned = 0;
     }
 
-    public void Update()
-    {   
+    private void Start() {
         moneyEarnedText.text = "" + moneyEarned;
         levelText.text = "" + mainLevel;
         timerText.text = "" + timeText;
         enemiesDefeatedText.text = "" + enemiesDefeated;
     }
+   
 
 }
