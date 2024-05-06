db16ac6e637949e21e4d13b9d5a7be440dc33e26
ElvisSkl <elvisforsell@gmail.com>
2024-05-06 14:38:18 +0200

    fixes to pushaway and some other stuff
    slight boss changes and fixes to pushaway

--- Assets/Scripts/Items/Utilities/PushAwayAbility.cs
+++ Assets/Scripts/Items/Utilities/PushAwayAbility.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PushAwayAbility : MonoBehaviour
+public class PushAwayAbility : Utility
 {
   
-    public float pushForce = 10f;
-    public float pushInterval = 3f;
+    public float pushForce = 5f;
+    public float pushInterval = 10f;
 
     private float timer = 0f;
+
+
+    [SerializeField] private float radiusIncreasePercentage;
+    [SerializeField] private float forceIncreasePercentage;
+
+    public int radiusRank;
+    public int forceRank;
+
 
     private void Update()
     {
@@ -19,9 +27,39 @@ public class PushAwayAbility : MonoBehaviour
         if (timer >= pushInterval)
         {
             PushEnemiesAway();
-            timer = 0f; 
+            timer = 0f;
         }
     }
+
+    public void IncreaseRadius()
+    {
+        radiusRank++;
+        gameObject.transform.localScale *= (1 + (radiusIncreasePercentage / 100f));
+    }
+
+    public void IncreaseForce()
+    {
+        radiusRank++;
+        pushForce *= (1 + (forceIncreasePercentage / 100f));
+    }
+
+    public float GetRadiusUpgradePercentage()
+    {
+        return radiusIncreasePercentage;
+    }
+
+
+    public float GetForceUpgradePercentage()
+    {
+        return forceIncreasePercentage;
+    }
+    protected override void CreateUpgradeOptions()
+    {
+        upgradeOptions.Add("IncreaseRadius");
+        upgradeOptions.Add("IncreaseForce");
+    }
+
+    
 
     private void PushEnemiesAway()
     {
