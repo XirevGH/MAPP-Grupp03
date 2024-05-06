8d17647db7dd0d724f0a22dac8857aeb242d4ea3
BrownCheeseLord <lovewilhelm@gmail.com>
2024-05-06 16:46:55 +0200

    Upgrade panel script changed

--- Assets/Scripts/Player/UpgradePanel.cs
+++ Assets/Scripts/Player/UpgradePanel.cs
@@ -8,27 +8,21 @@ public class UpgradePanel : MonoBehaviour
     {
         Time.timeScale = 0f;
         gameObject.gameObject.SetActive(true);
-<<<<<<< Updated upstream
         TriggerController.Instance.ToggleTrigger();
         SoundManager.Instance.ToggleMusicPause();
-=======
         tintPanel.SetActive(true);
         TriggerController.Instance.GetComponent<TriggerController>().ToggleTrigger();
         SoundManager.Instance.GetComponent<SoundManager>().ToggleMusicPause();
->>>>>>> Stashed changes
     }
 
     public void CloseUpgradeWindow()
     {
         Time.timeScale = 1f;
         gameObject.gameObject.SetActive(false);
-<<<<<<< Updated upstream
         TriggerController.Instance.ToggleTrigger();
         SoundManager.Instance.ToggleMusicPause();
-=======
         tintPanel.SetActive(false);
         TriggerController.Instance.GetComponent<TriggerController>().ToggleTrigger();
         SoundManager.Instance.GetComponent<SoundManager>().ToggleMusicPause();
->>>>>>> Stashed changes
     }
 }
