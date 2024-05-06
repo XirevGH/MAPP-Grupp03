6786db7f0ad2f496ff6aeecbbe9cae8b9df85f35
XirevGH <dcz95@hotmail.com>
2024-05-06 11:33:00 +0200

    Removed unused code

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -7,9 +7,7 @@ public abstract class Item : MonoBehaviour
     [SerializeField] protected Player player;
     [SerializeField] protected string itemName;
     protected List<string> upgradeOptions = new List<string>();
-    private bool initialUpgradesComplete = false;
     protected bool active;
-    
 
     private void LateUpdate()
     {
