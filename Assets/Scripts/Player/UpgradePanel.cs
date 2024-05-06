d48ace379894889584098017d59c6982db517332
BrownCheeseLord <lovewilhelm@gmail.com>
2024-05-06 16:42:52 +0200

    Upgrade panel changes

--- Assets/Scripts/Player/UpgradePanel.cs
+++ Assets/Scripts/Player/UpgradePanel.cs
@@ -2,19 +2,33 @@ using UnityEngine;
 
 public class UpgradePanel : MonoBehaviour
 {
+    [SerializeField] private GameObject tintPanel;
+
     public void OpenUpgradeWindow()
     {
         Time.timeScale = 0f;
         gameObject.gameObject.SetActive(true);
+<<<<<<< Updated upstream
         TriggerController.Instance.ToggleTrigger();
         SoundManager.Instance.ToggleMusicPause();
+=======
+        tintPanel.SetActive(true);
+        TriggerController.Instance.GetComponent<TriggerController>().ToggleTrigger();
+        SoundManager.Instance.GetComponent<SoundManager>().ToggleMusicPause();
+>>>>>>> Stashed changes
     }
 
     public void CloseUpgradeWindow()
     {
         Time.timeScale = 1f;
         gameObject.gameObject.SetActive(false);
+<<<<<<< Updated upstream
         TriggerController.Instance.ToggleTrigger();
         SoundManager.Instance.ToggleMusicPause();
+=======
+        tintPanel.SetActive(false);
+        TriggerController.Instance.GetComponent<TriggerController>().ToggleTrigger();
+        SoundManager.Instance.GetComponent<SoundManager>().ToggleMusicPause();
+>>>>>>> Stashed changes
     }
 }
