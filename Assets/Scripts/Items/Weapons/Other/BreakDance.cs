7dd20c327736048ad886980f6825ae6f1122d416
BrownCheeseLord <lovewilhelm@gmail.com>
2024-05-18 00:25:04 +0200

    New BreakDance ability
    StagePresence and old BreakDance gone

--- Assets/Scripts/Items/Weapons/Other/BreakDance.cs
+++ Assets/Scripts/Items/Weapons/Other/BreakDance.cs
@@ -1,63 +1,79 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class BreakDance : PhysicalWeapon
+public class BreakDance : Weapon
 {
-    public bool dealDamage;
-    public int amountOfHits;
-    public bool usedAbility = false;
-    private int hitsCounter;
-    Collider2D other;   
+
+    [SerializeField] private float radiusIncreasePercentage;
+
+    public int radiusRank;
+    public int radiusUpgradeCost;
+
+    private HashSet<GameObject> enemies = new HashSet<GameObject>();
+
+    private void Start()
+    {
+        UnityAction action = new UnityAction(Attack);
+        TriggerController.Instance.SetTrigger(0, action);
+    }
+
+    public void IncreaseRadius()
+    {
+        radiusRank++;
+        gameObject.transform.localScale *= (1 + (radiusIncreasePercentage / 100f));
+    }
+
+    public float GetRadiusIncreasePercentage()
+    {
+        return radiusIncreasePercentage;
+    }
+
+    public float GetCurrentRadiusIncrease()
+    {
+        return (float)Math.Round((Mathf.Pow(1 + (radiusIncreasePercentage / 100f), radiusRank) - 1) * 100, 1);
+    }
+
+    public int GetIncreaseRadiusCost()
+
+    {
+        return radiusUpgradeCost;
+    }
+
+    public int GetRadiusUpgradeRank()
+    {
+        return radiusRank;
+    }
+
+    protected override void CreateUpgradeOptions()
+    {
+        base.CreateUpgradeOptions();
+        upgradeOptions.Add("IncreaseRadius");
+    }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        //if (WeaponIsReady()) {
-        //    hitsCounter = amountOfHits;
-        //    while (hitsCounter > 0)
-        //        {
-        //            BreakDanceInterval();
-        //        }
-        //    StartCooldown();
-        //    if (hitsCounter <= 0) 
-        //    {
-        //    usedAbility = true;
-        //    }
-        //}
-    }
-
-    private void BreakDanceInterval()
-    {
-        if (!dealDamage) 
+        if (other.CompareTag("Enemy"))
         {
-            dealDamage = true;
-            Invoke("BreakDanceDamage", 0.5f);
+            enemies.Add(other.gameObject);
         }
     }
 
-    private void BreakDanceDamage()
+    private void OnTriggerExit2D(Collider2D other)
     {
-
-        DealDamage(other);
-        hitsCounter--;
-      //  GetComponent<SpriteRenderer>().enabled = true;
-        dealDamage = false;
-        Invoke("RemoveIndicator", 0.5f);
-     //   Invoke("AbilityCooldown", 3f);
-    }
-
-    private void RemoveIndicator()
-    {
-     //   GetComponent<SpriteRenderer>().enabled = false;
-    }
-
-    private void AbilityCooldown()
-    {
-      usedAbility = false;
+        enemies.Remove(other.gameObject);
     }
 
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        foreach(GameObject enemy in enemies) 
+        {
+            if(enemy.gameObject != null)
+            {
+                enemy.GetComponent<Enemy>().TakeDamage(damage);
+            }
+        }
     }
 }
