0ab6dee743b2e09421985c8b0340e8bcb96c1572
Love Myhre Collins <lovewilhelm@gmail.com>
2024-05-26 08:37:34 +0200

    First version of light flicker

--- Assets/Scripts/UI/MainMenuEffectController.cs
+++ Assets/Scripts/UI/MainMenuEffectController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuEffectController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Image[] buttonImages;
+    [SerializeField] private Sprite playLit, playUnlit, upgradesLit, upgradesUnlit, settingsLit, settingsUnlit;
+    [SerializeField] private float minWaitTime, maxWaitTime, minFlickerTime, maxFlickerTime;
+
     void Start()
     {
-        
+        StopAllCoroutines();
+        StartCoroutine(FlickerLights());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FlickerLights()
     {
-        
+        float randTime = Random.Range(minWaitTime, maxWaitTime);
+        yield return new WaitForSeconds(randTime);
+        int randButton = Random.Range(0, buttonImages.Length);
+        if(randButton == 0)
+        {
+            buttonImages[0].sprite = playUnlit;
+            yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+            buttonImages[0].sprite = playLit;
+        }
+        if (randButton == 1)
+        {
+            buttonImages[1].sprite = upgradesUnlit;
+            yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+            buttonImages[1].sprite = upgradesLit;
+        }
+        if (randButton == 2)
+        {
+            buttonImages[2].sprite = settingsUnlit;
+            yield return new WaitForSeconds(Random.Range(minFlickerTime, maxFlickerTime));
+            buttonImages[2].sprite = settingsLit;
+        }
+        StartCoroutine(FlickerLights());
     }
 }
