7c3695061219ff60b1ff33eb61ede30ab463a00f
XirevGH <74151509+XirevGH@users.noreply.github.com>
2026-09-16 12:28:05 +0200

    Despawn out-of-bounds tiles; optimize Floor
    Add despawnBuffer setting and a RemoveOutOfBoundsTiles method to delete tiles outside the camera bounds plus a margin, preventing uncontrolled Tilemap growth. Use tilemap.HasTile for placement checks, set TileFlags.None when placing tiles, and call CompressBounds after removals. Also rename local bounds variable to cameraBounds and remove an unused using. These changes improve performance and memory usage by cleaning up distant tiles.

--- Assets/Scripts/GameFunctions/Floor.cs
+++ Assets/Scripts/GameFunctions/Floor.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEngine.UIElements;
 
 public class Floor : MonoBehaviour
 {
-    
     public Tilemap tilemap;
     public TileBase tileToPlace;
     public Color[] colors;
     public GameController gameController;
+
+    [Header("Settings")]
+    [Tooltip("Extra margin (in tiles) outside the camera before tiles get deleted.")]
+    public int despawnBuffer = 2;
 
     void PlaceTile(Vector3Int position)
     {
@@ -18,6 +20,7 @@ public class Floor : MonoBehaviour
         {
             tilemap.SetTile(position, tileToPlace);
             tilemap.SetTileFlags(position, TileFlags.None);
+
             int colorChoice = Random.Range(0, colors.Length * 2);
             if (colorChoice >= colors.Length)
             {
@@ -31,31 +34,50 @@ public class Floor : MonoBehaviour
                 }
             }
             tilemap.SetColor(position, colors[colorChoice]);
-        } 
+        }
         else
         {
             Debug.LogError("Tilemap or tile to place is null!");
         }
     }
 
-
-
-
     private void Update()
     {
-        BoundsInt bounds = gameController.GetBoundsFromCamera();
-        for (int x = bounds.min.x; x <= bounds.max.x; x++)
+        BoundsInt cameraBounds = gameController.GetBoundsFromCamera();
+
+        for (int x = cameraBounds.min.x; x <= cameraBounds.max.x; x++)
         {
-            for (int y = bounds.min.y; y <= bounds.max.y; y++)
+            for (int y = cameraBounds.min.y; y <= cameraBounds.max.y; y++)
             {
                 Vector3Int position = new Vector3Int(x, y, 0);
-                TileBase tile = tilemap.GetTile(position);
-                if (tile == null)
+                if (!tilemap.HasTile(position))
                 {
                     PlaceTile(position);
-                    
                 }
             }
         }
+
+        RemoveOutOfBoundsTiles(cameraBounds);
+    }
+
+    private void RemoveOutOfBoundsTiles(BoundsInt cameraBounds)
+    {
+        int minX = cameraBounds.min.x - despawnBuffer;
+        int maxX = cameraBounds.max.x + despawnBuffer;
+        int minY = cameraBounds.min.y - despawnBuffer;
+        int maxY = cameraBounds.max.y + despawnBuffer;
+
+        foreach (Vector3Int pos in tilemap.cellBounds.allPositionsWithin)
+        {
+            if (tilemap.HasTile(pos))
+            {
+                if (pos.x < minX || pos.x > maxX || pos.y < minY || pos.y > maxY)
+                {
+                    tilemap.SetTile(pos, null);
+                }
+            }
+        }
+
+        tilemap.CompressBounds();
     }
 }
